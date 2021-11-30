138b44c59e16978d25efbde05041349ce8952380
tony.yoo <l22491360@gmail.com>
2021-11-30 14:19:29 +0000

    add unittest

--- test/download/movielen/test_ml.py
+++ test/download/movielen/test_ml.py
@@ -2,11 +2,16 @@ import shutil
 import unittest
 from os.path import exists
 
-from agtool.download.movielen import ml_100k
+from agtool.download.movielen import ml_100k, ml_10m
 
 
 class MovieLen(unittest.TestCase):
     def test01_ml_100k(self):
         DIR = ml_100k()
+        self.assertTrue(exists(DIR))
+        shutil.rmtree(DIR)
+
+    def test01_ml_10m(self):
+        DIR = ml_10m()
         self.assertTrue(exists(DIR))
         shutil.rmtree(DIR)
