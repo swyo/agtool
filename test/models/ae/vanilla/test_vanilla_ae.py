6ee27bfc2de255c4ea8ab28ecf476fdd745c3b18
tony.yoo <l22491360@gmail.com>
2021-12-02 06:29:52 +0000

    add unittest for autoencoder, but accuracy test not added

--- test/models/ae/vanilla/test_vanilla_ae.py
+++ test/models/ae/vanilla/test_vanilla_ae.py
@@ -6,3 +6,11 @@ from agtool.models.ae.vanilla import DeepAutoEncoder
 class TestAE(unittest.TestCase):
     def test01_init(self):
         model = DeepAutoEncoder()
+
+    def test02_train(self):
+        model = DeepAutoEncoder()
+        model.fit(epochs=10)
+
+    def test03_analysis(self):
+        model = DeepAutoEncoder()
+        model.analysis()
