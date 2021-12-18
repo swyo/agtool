b92b245a0dc487a56cceea6e98121b4efa5f877b
tony.yoo <l22491360@gmail.com>
2021-12-18 12:51:10 +0000

    update positive random walk with sepecial case, work length=1

--- agtool/models/gnn/sage/model.py
+++ agtool/models/gnn/sage/model.py
@@ -31,9 +31,8 @@ class NeighborSampler(RawNeighborSampler):
 
     def sample(self, batch):
         batch = torch.tensor(batch)
-        row, col, _ = self.adj_t.coo()
-        # For each node in `batch`, we sample a direct neighbor (as positive
-        # example) and a random node (as negative example):
+        # For each node in `batch`, we sample a direct neighbor (as positive example) and a random node (as negative example):
+        row, col, _ = self.adj_t[batch].coo()
         pos_batch = random_walk(row, col, batch, walk_length=1, coalesced=False)[:, 1]
         neg_batch = self.neg_samples[batch].view(-1)
         batch = torch.cat([batch, pos_batch, neg_batch], dim=0)
